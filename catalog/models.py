19826eabf5a246b7b1666a479d1728909c82a04c
youichiro <cinnamon416@gmail.com>
2021-07-09 02:57:16 +0900

    Genreのadmin表示を変更

--- catalog/models.py
+++ catalog/models.py
@@ -46,6 +46,11 @@ class Book(models.Model):
 
   def get_absolute_url(self):
     return reverse('book-detail', args=[str(self.id)])
+
+  def display_genre(self):
+    return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+  display_genre.short_description = 'Genre'
   
 
 class BookInstance(models.Model):
