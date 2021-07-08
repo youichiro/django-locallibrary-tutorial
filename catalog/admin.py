7ced8f7cc9f0fe0b8d20feebc2730d85b913b130
youichiro <cinnamon416@gmail.com>
2021-07-09 02:57:41 +0900

    adminの表示を変更

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import Author, Genre, Language, Book, BookInstance
 
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(Language)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+  pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+  pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('title', 'author', 'display_genre')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+  list_filter = ('status', 'due_back')
